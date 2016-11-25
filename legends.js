91630aa811b5a84e0ad9ce2f69f860e7108894bb
Fabio Pampin <fpampin@gm2dev.com>
2016-11-25 19:55:37 -0300

    Implement custom legend rather than using canvas

--- legends.js
+++ legends.js
@@ -2,8 +2,13 @@ for (var i = 0; typeof window["data"+i] != 'undefined'; i++) {
   var data = window["data"+i].datasets;
   var dom = $('#legend-container-' + i);
   $.each(data, function( k, v ) {
+    console.log( v );
     dom.append(
-      '<span class="color" style="background-color:'+v.backgroundColor+';"></span>'
+      '<span class="legend">' +
+        '<span class="color" style="background-color:' + v.backgroundColor + ';"></span>' +
+        '<span class="border" style="width:' + v.borderWidth + 'px;"></span>' +
+        '<span class="text">' + v.label + '</span>' +
+      '</span>'
     );
     // console.table( v );
   });
